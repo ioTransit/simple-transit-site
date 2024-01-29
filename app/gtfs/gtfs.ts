0bbdaf9071636594764a1f48e476155bb6bf3b00
Walter Jenkins <walter.k.jenkins@gmail.com>
2024-01-28 20:51:55 -0600

    remove tinacms

--- app/gtfs/gtfs.ts
+++ app/gtfs/gtfs.ts
@@ -1,16 +1,12 @@
-import { writeFile } from "fs/promises";
-
 import { importGtfs } from "gtfs";
 // @ts-expect-error no types
 import gtfsToHtml from "gtfs-to-html";
 
 import { gtfsConfig } from "../config";
-import { getAllRoutes } from "../models/routes.server";
 
 export const load = async () => {
   try {
     await importGtfs(gtfsConfig);
-    const routes = await getAllRoutes();
     gtfsToHtml(gtfsConfig)
       .then(() => {
         console.log("HTML Generation Successful");
@@ -21,12 +17,6 @@ export const load = async () => {
         console.error(err);
         process.exit(1);
       });
-    for (const route of routes) {
-      await writeFile(
-        `routes/${route.routeShortName}.md`,
-        `\nThis is where the text goes`,
-      );
-    }
   } catch (error) {
     console.error(error);
   }
--- routes/DRX.md
+++
@@ -1,2 +0,0 @@
-
-This is where the text goes
