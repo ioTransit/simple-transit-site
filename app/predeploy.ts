2739d3f479a975c1d08fcfb243b60a5d88f79302
Walter Jenkins <walter.k.jenkins@gmail.com>
2024-02-03 10:52:00 -0600

    move predeploy to app/

--- app/predeploy.ts
+++ app/predeploy.ts
@@ -1,25 +1,26 @@
 import bcrypt from "bcryptjs";
 import { eq } from "drizzle-orm";
-import z from "zod";
 
-import { db } from "./config";
-import { password, user } from "./schema";
+import { db } from "drizzle/config";
+import { password, user } from "drizzle/schema";
 
-const config = () => {
-  const _config = z
-    .object({
-      ADMIN_EMAIL: z.string(),
-      ADMIN_PASSWORD: z.string(),
-      DATABASE_URL: z.string(),
-      AGENCY_NAME: z.string(),
-      MAPBOX_ACCESS_TOKEN: z.string(),
-      GTFS_URL: z.string(),
-    })
-    .parse(process.env);
-  return _config;
-};
+import { envConfig } from "./config.server";
 
-const envConfig = config();
+// const config = () => {
+//   const _config = z
+//     .object({
+//       ADMIN_EMAIL: z.string(),
+//       ADMIN_PASSWORD: z.string(),
+//       DATABASE_URL: z.string(),
+//       AGENCY_NAME: z.string(),
+//       MAPBOX_ACCESS_TOKEN: z.string(),
+//       GTFS_URL: z.string(),
+//     })
+//     .parse(process.env);
+//   return _config;
+// };
+//
+// const envConfig = config();
 async function predeploy() {
   const email = envConfig.ADMIN_EMAIL;
 
