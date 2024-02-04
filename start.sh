6a45ca1327e49526a77f59659eea853d261c7df8
Walter Jenkins <walter.k.jenkins@gmail.com>
2024-02-04 15:57:10 -0600

    add back wal

--- start.sh
+++ start.sh
@@ -15,7 +15,7 @@
 
 # npx prisma migrate deploy
 ## Enables WAL Mode
-# npm run enable-wal
+npm run enable-wal
 
 # npx concurrently "litestream replicate -config /etc/litestream.yml" "npm run start"
 exec litestream replicate -exec "npm run start" /drizzle/data.db s3://simple-transit-site/data.db
