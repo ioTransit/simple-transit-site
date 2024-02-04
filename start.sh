89bad262cd8a25ff91d4429895ba7b7b8e638650
Walter Jenkins <walter.k.jenkins@gmail.com>
2024-02-04 17:00:23 -0600

    remove wal

--- start.sh
+++ start.sh
@@ -15,7 +15,6 @@
 
 # npx prisma migrate deploy
 ## Enables WAL Mode
-npm run enable-wal
 
 npx concurrently "npm run backup" "npm run start"
 # exec litestream replicate -exec "npm run start" /drizzle/data.db s3://simple-transit-site/data.db
