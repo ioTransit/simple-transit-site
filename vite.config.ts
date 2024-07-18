40e05b44bced9381fccb74da1a712a32d115bf7b
Walter Jenkins <walter@transit.chat>
2024-07-18 14:20:10 -0500

    will it build?

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { installGlobals } from "@remix-run/node";
+
+installGlobals();
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [
     remix({
       future: {
